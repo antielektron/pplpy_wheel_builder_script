20cffaefb114aef36e5fb986dd6e694e493c772a
Jonas Weinz <s6jowein@uni-bonn.de>
2018-05-15 21:56:05 +0200

    v0.2

--- README.md
+++ README.md
@@ -1,3 +1,73 @@
-# pplpy_wheel_builder
+# PPLPY WHEEL BUILDER
 
-Tools to generate precompiled python wheels for [pplpy](https://github.com/videlec/pplpy) and it's dependencies
+this script generates three precompiled wheels: 
+* One for pplpy 
+* One containing the libraries pplpy depends on
+* One for gmpy2 
+
+Currently it only runs on debian based distributions because it's using `apt` to install the build dependencies *(but porting this to other distros should be easy)*
+
+### Prerequisites
+
+* just a fresh Ubuntu installation (currently only tested on 16.04 LTS, but it should run also on newer versions)
+
+### Download
+
+* Precompiled wheels for the amd64 architecture (linux) and the builder script itself can be found [here](https://the-cake-is-a-lie.net/gogs/jonas/python_polyhedron/releases)
+
+### How to use the script
+
+* download this script by cloning this repo or [download](https://the-cake-is-a-lie.net/gogs/jonas/python_polyhedron/releases) and extract it
+
+* change to the scripts directory and run
+
+  ```bash
+  ./pplpy_wheel_builder.sh
+  ```
+
+* The generated wheels can be found in the `generated_wheels` folder
+
+### installing the wheels
+
+* make sure `cython` and `cysignals` are installed:
+
+  ```bash
+  pip3 install cython cysignals --user
+  ```
+
+  * or system wide with
+
+    ```bash
+    sudo -H pip3 install cython cysignals 
+    ```
+
+    
+
+* run
+
+  ```bash
+  pip3 install *.whl --user
+  ```
+
+  * or system wide:
+
+    ```
+    sudo -H pip3 install *.whl
+    ```
+
+    
+
+* **NOTE**: you have to adjust your `LD_LIBRARY_PATH` otherwise the installed libraries are not found at runtime. To do this add the line
+
+  ```bash
+  export LD_LIBRARY_PATH=~/.local/lib/:$LD_LIBRARY_PATH
+  ```
+
+  * or if the packages are installed system wide:
+
+    ```bash
+    export LD_LIBRARY_PATH=/usr/local/lib/:$LD_LIBRARY_PATH
+    ```
+
+  at the end of your `~/.bashrc` (or your `.zshrc` or whatever fancy shell you use)
+
---
+++ pplpy_wheel_builder.sh
@@ -0,0 +1,346 @@
+#!/usr/bin/env bash
+
+# WARNING: this script is still under construction!!!
+
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
+
+WHEEL_DEPS_PATH="${SCRIPTPATH}/pplpy_wheel_deps"
+
+WHEELS_PATH="${SCRIPTPATH}/generated_wheels"
+
+function lineprint {
+    printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' =
+}
+
+function message {
+    lineprint
+    printf "$1\n"
+    lineprint
+}
+
+current_action="IDLE"
+
+function confirm_action {
+    message "successfully finished action: $current_action"
+}
+
+function set_action {
+    current_action="$1"
+    message "$1"
+}
+
+function perform {
+    "$@"
+    local status=$?
+    if [ $status -ne 0 ]
+    then
+        message "$current_action failed!"
+    fi
+    return $status
+}
+
+function perform_and_exit {
+    perform "$@" || exit
+}
+
+# some variables and tags for Versions (using git or mecurial tags here!)
+GMP_VERSION="gmp-6.1.0"
+PPL_VERSION="1.2"       # not used, since the ppl repo does not declares git-tags for their published versions (?!?)
+MPFR_VERSION="4.0.1"
+MPC_VERSION="1.1.0"
+GMPY_VERSION="gmpy2-2.1.0a1"
+PPLPY_VERSION="0.7"
+
+PYTHON_ENV="python3"
+PYTHON_PIP="pip3"
+
+# setting up our environment:
+export LD_LIBRARY_PATH=~/local/lib:~/.local/lib/:$LD_LIBRARY_PATH
+export C_INCLUDE_PATH=~/local/include:$C_INCLUDE_PATH
+export CPLUS_INCLUDE_PATH=~/local/include:$CPLUS_INCLUDE_PATH
+export LIBRARY_PATH=$LD_LIBRARY_PATH
+
+# if not exists: create ~/src and ~/local
+mkdir -p ~/src
+mkdir -p ~/local
+
+echo "checking whether we're on a distro with apt-package-management..."
+
+apt --version
+
+if [ $? -ne 0 ]
+then
+    message "seems we're not on a debian based distribution. Checking for dnf"
+
+    dnf --version
+
+    if [ $? -ne 0 ]
+    then
+
+        message "no apt or dnf package manager found. skipping installing dependencies"
+
+    else
+
+        set_action "installing build dependencies"
+        perform_and_exit sudo dnf install git python3-pip python3-devel python-pip wget gcc-c++ gcc-gfortran mercurial libtool bison texinfo autoconf
+
+        # fixing strange default fedora flags
+        export CFLAGS=-Wp,-U_FORTIFY_SOURCE
+
+        confirm_action
+    fi
+
+
+else
+    # installing build dependencies
+    set_action "installing build dependencies"
+
+    perform_and_exit sudo apt install git python3-pip python3-dev python-pip wget build-essential mercurial libtool bison texinfo autoconf
+    confirm_action
+fi
+
+if [ -e ~/local/lib/libgmp.so ]
+then
+    message "found existing GMP library in ~/local/lib/"
+else
+
+    # download and compile GMP
+    set_action "download GMP sources for version $GMP_VERSION"
+
+    perform_and_exit cd ~/src
+    # removing existing sources
+    rm -rf ./gmp/
+
+    # checking out correct version
+    perform_and_exit hg clone https://gmplib.org/repo/gmp/ ./gmp
+    cd gmp
+
+    perform_and_exit hg up $GMP_VERSION
+
+    confirm_action
+    set_action "compile GMP $GMP_VERSION"
+
+    perform_and_exit ./.bootstrap
+    perform_and_exit ./configure --prefix=`realpath ~/local` --enable-cxx --enable-fat
+    perform_and_exit make -j4
+    perform_and_exit make install
+
+    confirm_action
+
+fi
+
+if [ -e ~/local/lib/libppl.so ]
+then
+    message "found existing PPL library in ~/local/lib/"
+else
+
+    set_action "download PPL sources for version $PPL_VERSION"
+
+    perform_and_exit cd ~/src/
+
+    rm -rf ./ppl/
+
+    perform_and_exit git clone git://git.cs.unipr.it/ppl/ppl.git
+
+    confirm_action
+    set_action "compile PPL $PPL_VERSION"
+
+    perform_and_exit cd ppl/
+    perform_and_exit autoreconf
+    perform_and_exit ./configure --prefix=`realpath ~/local` --disable-documentation
+    perform_and_exit make -j4
+    perform_and_exit make install
+
+    confirm_action
+fi
+
+
+if [ -e ~/local/lib/libmpfr.so ]
+then
+    message "found existing MPFR library in ~/local/lib/"
+else
+
+    set_action "download MPFR sources for version $MPFR_VERSION"
+
+    perform_and_exit cd ~/src
+
+    perform_and_exit rm -rf mpfr-${MPFR_VERSION} # cleaning up
+
+    perform_and_exit wget http://www.mpfr.org/mpfr-current/mpfr-${MPFR_VERSION}.tar.xz
+    perform_and_exit tar -xf mpfr-${MPFR_VERSION}.tar.xz
+
+    # cleaning up
+    perform_and_exit rm mpfr-${MPFR_VERSION}.tar.xz
+
+    confirm_action
+    set_action "compile MPFR $MPFR_VERSION"
+
+    perform_and_exit cd mpfr-${MPFR_VERSION}
+
+    perform_and_exit ./configure --prefix=$HOME/local --with-gmp=$HOME/local
+    perform_and_exit make -j4
+    perform_and_exit make install
+
+    confirm_action
+fi
+
+if [ -e ~/local/lib/libmpc.so ]
+then
+    message "found existing MPC library in ~/local/lib/"
+else
+
+    set_action "download MPC sources for version $MPC_VERSION"
+
+    perform_and_exit cd ~/src
+    perform_and_exit wget https://ftp.gnu.org/gnu/mpc/mpc-${MPC_VERSION}.tar.gz
+    perform_and_exit tar -xf mpc-${MPC_VERSION}.tar.gz
+
+    # cleaning up
+    perform_and_exit rm mpc-${MPC_VERSION}.tar.gz
+
+    perform_and_exit cd mpc-${MPC_VERSION}
+
+    confirm_action
+    set_action "compile MPC $MPC_VERSION"
+
+    perform_and_exit ./configure --prefix=$HOME/local --with-gmp=$HOME/local --with-mpfr=$HOME/local
+    perform_and_exit make -j4
+    perform_and_exit make install
+
+    confirm_action
+
+fi
+
+set_action "looking for installed wheels package and install it if neccessary"
+
+$PYTHON_PIP show wheel
+pip_status=$?
+
+if [ $pip_status -ne 0 ]
+then
+    $PYTHON_PIP install wheel --user
+else
+    message "found existing wheel in: $($PYTHON_PIP show wheel | grep Location | awk '{print $2}')"  
+fi
+
+confirm_action
+
+
+set_action "download GMPY sources for version $GMPY_VERSION"
+
+perform_and_exit cd ~/src/
+
+perform_and_exit rm -rf gmpy
+
+perform_and_exit git clone https://github.com/aleaxit/gmpy.git
+
+confirm_action
+
+perform_and_exit cd gmpy/
+perform_and_exit git checkout $GMPY_VERSION
+
+confirm_action
+set_action "patching gmpy's setup.py to make it compatible with python-wheel"
+
+perform_and_exit git apply < $SCRIPTPATH/gmpy.patch
+
+confirm_action
+set_action "compile, build_wheel and install GMPY $GMPY_VERSION with git"
+
+export CFLAGS="-I~/local/include/ -L~/local/lib/ $CFLAGS"
+
+perform_and_exit /usr/bin/env $PYTHON_ENV setup.py bdist_wheel
+perform_and_exit /usr/bin/env $PYTHON_ENV setup.py install --prefix=$HOME/.local
+
+confirm_action
+
+
+confirm_action
+set_action "looking for cython and cysignals and install them if needed"
+
+$PYTHON_PIP show cython
+pip_status=$?
+
+if [ $pip_status -ne 0 ]
+then
+    $PYTHON_PIP install cython --user
+else
+    message "found existing cython in: $($PYTHON_PIP show cython | grep Location | awk '{print $2}')"  
+fi
+
+pip3 show cysignals
+pip_status=$?
+
+if [ $pip_status -ne 0 ]
+then
+    $PYTHON_PIP install cysignals --user
+else
+    message "found existing cysignals in: $($PYTHON_PIP show cysignals | grep Location | awk '{print $2}')"
+fi
+
+confirm_action
+
+# now setup a list of external libraries to include in our wheel build and create something like 
+# a 'pure dependency package' for the pplpy-wheel
+
+set_action "create binary dependency wheel for pplpy"
+
+perform_and_exit cd $WHEEL_DEPS_PATH
+
+# copy all needed libraries in here
+
+perform_and_exit cp ~/local/lib/*.so ./
+perform_and_exit cp ~/local/lib/*.so.* ./
+
+# and building package:
+
+perform_and_exit /usr/bin/env $PYTHON_ENV setup.py bdist_wheel
+
+# clean up:
+
+perform_and_exit rm -rf ./*.so
+
+confirm_action
+set_action "download pplpy sources"
+
+perform_and_exit cd ~/src
+
+# setting up cflags:
+export CFLAGS="-I~/local/include/ -L~/local/lib/ $CFLAGS"
+
+perform_and_exit rm -rf ./pplpy
+
+perform_and_exit git clone https://github.com/videlec/pplpy.git
+
+perform_and_exit cd pplpy
+
+perform_and_exit git checkout $PPLPY_VERSION
+
+confirm_action
+
+set_action "patching pplpy's setup.py"
+
+perform_and_exit git apply < $SCRIPTPATH/pplpy.patch
+
+confirm_action
+set_action "create wheel for pplpy"
+
+/usr/bin/env $PYTHON_ENV setup.py bdist_wheel
+
+confirm_action
+
+# copy built wheels to working directory
+
+set_action "copy built wheels to $WHEELS_PATH"
+
+perform_and_exit rm -rf $WHEELS_PATH
+perform_and_exit mkdir $WHEELS_PATH
+perform_and_exit cp $WHEEL_DEPS_PATH/dist/*.whl "$WHEELS_PATH/"
+perform_and_exit cp ~/src/pplpy/dist/*.whl "$WHEELS_PATH/"
+perform_and_exit cp ~/src/gmpy/dist/*.whl "$WHEELS_PATH/"
+
+confirm_action
+
+message "...and we're done!\nGenerated wheels can be found in $WHEEL_DEPS_PATH\nTo install a wheel file just run: $PYTHON_PIP install wheel_file.whl --user\nHave a nice day :-)"
+
